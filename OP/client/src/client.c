51453793a632e119c597b10796ceacab48f9d126
Ostap Pokornyi <opokornyi@e1r1p6.unit.ua>
2021-02-28 22:14:42 +0200

    default server with Default client withoute threads

--- OP/client/src/client.c
+++ OP/client/src/client.c
@@ -36,3 +36,53 @@ int main(int ac, char **av) {
 
     return (0);
 }
+// #include "chat.h"
+
+// int main (int argc, char *argv[]) {
+//     int sock_fd = 0;
+//     int ip = 0;
+//     char recvBuff[1024];
+//     struct sockaddr_in client;
+//     if(argc != 2){
+//         printf("usage %s ip of server", argv[0]);
+//         return 1;
+//     }
+//     if((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+//         printf("Error socket ");
+//         return 1;
+//     }
+//     memset(recvBuff, '0',sizeof(recvBuff));
+//     memset(&client, '0', sizeof(client));
+
+//     client.sin_family = AF_INET;
+//     client.sin_addr.s_addr = htonl(INADDR_ANY);
+//     client.sin_port = htons(5000);
+    
+//     if(inet_pton(AF_INET, argv[1], &client.sin_addr) <= 0){
+//         printf("inet_pton error");
+//         return 1;
+//     }
+    
+//     if(connect(sock_fd, (struct sockaddr *) &client, sizeof(client)) < 0 ){
+//        printf("Error connect failed");
+//        return 1; 
+//     }
+
+//     while ((ip = read(sock_fd, recvBuff, sizeof(recvBuff)-1)) > 0)
+//     {
+//         printf("ONE\n");
+//         recvBuff[ip] = 0;
+//         if(fputs(recvBuff, stdout) == EOF)
+//         {
+//             printf("\n Error : Fputs error\n");
+//             printf("ONE1\n");
+//         }
+//     }
+//     printf("ONE2\n");
+//     if(ip < 0)
+//     {
+//         printf("ONE3\n");
+//         printf("\n Read error \n");
+//     }
+//     printf("ONE4\n");
+// }
--- OP/server/src/server.c
+++ OP/server/src/server.c
@@ -1,9 +1,32 @@
 #include "server.h"
 
-int main(int ac, char **av) {
-    if (ac != 3) {
-        printf("ERROR!");
+int main (int argc, char *argv[]) {
+
+
+    int port = 0;
+    time_t time;
+    char sendBuff[1025];
+    int listen_fd = 0;
+    struct sockaddr_in addr;
+    listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(sendBuff, '0', sizeof(sendBuff));
+    memset(&addr, '0', sizeof(addr));
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(5000);
+    bind(listen_fd, (struct sockaddr *) &addr, sizeof(addr));
+    listen(listen_fd, 10);
+
+
+    while(1){
+        printf("start1\n");
+        port = accept(listen_fd, (struct sockaddr *) NULL, NULL);
+        snprintf(sendBuff, sizeof(sendBuff), "%s", ctime(&time));
+        write(port, sendBuff, strlen(sendBuff));   
+        printf("start2\n");
+        close(port);
+        sleep(1);
     }
 
-    return (0);
 }
